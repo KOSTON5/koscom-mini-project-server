e2e42443199e42e570f5fb51003c3726c4a7b7d0
Geonwoo Pack <packdev937@gmail.com>
2025-01-25 14:31:39 +0900

    feat (presentation) : 주문 매수/매도의 스켈레톤 코드를 작성한다

--- src/main/java/kr/co/koscom/miniproject/presentation/rest/OrderController.java
+++ src/main/java/kr/co/koscom/miniproject/presentation/rest/OrderController.java
@@ -2,24 +2,21 @@ package kr.co.koscom.miniproject.presentation.rest;
 
 import kr.co.koscom.miniproject.application.OrderApplicationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/order")
-public class OrderController {
+public class OrderController implements OrderControllerDocs {
 
     private final OrderApplicationService orderApplicationService;
 
-    @PostMapping("/sell")
-    public void sell() {
+    @Override
+    public void purchaseStock() {
 
     }
 
-    @PostMapping("/buy")
-    public void buy() {
+    @Override
+    public void sellStock() {
 
     }
 }
--- src/main/java/kr/co/koscom/miniproject/presentation/rest/OrderControllerDocs.java
+++ src/main/java/kr/co/koscom/miniproject/presentation/rest/OrderControllerDocs.java
@@ -1,5 +1,15 @@
 package kr.co.koscom.miniproject.presentation.rest;
 
-public interface OrderControllerDocs {
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "주문 API", description = "주식 주문 비즈니스 로직과 관련된 API 명세서입니다.")
+@RequestMapping("/api/order")
+public interface OrderControllerDocs {
+    @Operation(summary = "주식 매수", description = "주식을 매수합니다.")
+    void purchaseStock();
+
+    @Operation(summary = "주식 매도", description = "주식을 매도합니다.")
+    void sellStock();
 }
