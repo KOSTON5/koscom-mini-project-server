1ef9b81a2415d251a403cca244cb20e5994ea7da
Geonwoo Pack <packdev937@gmail.com>
2025-01-25 12:13:38 +0900

    feat (infrastructure) : WebFluxConfig를 작성한다

---
+++ src/main/java/kr/co/koscom/miniproject/infrastructure/WebFluxConfig.java
@@ -0,0 +1,22 @@
+package kr.co.koscom.miniproject.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+            .codecs(
+	clientCodecConfigurer ->
+	    clientCodecConfigurer
+	        .defaultCodecs()
+	        .maxInMemorySize(16 * 1024 * 1024)
+            )
+            .build();
+    }
+}
