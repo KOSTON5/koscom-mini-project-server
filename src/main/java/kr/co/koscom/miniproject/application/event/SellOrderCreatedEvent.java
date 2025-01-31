69ac5c437777bcaeb1c1dfeca6e90beb862baa16
Geonwoo Pack <packdev937@gmail.com>
2025-01-31 20:03:52 +0900

    feat (application) : 비동기 적으로 주문을 처리 하기 위해 Event 객체를 생성한다

---
+++ src/main/java/kr/co/koscom/miniproject/application/event/SellOrderCreatedEvent.java
@@ -0,0 +1,21 @@
+package kr.co.koscom.miniproject.application.event;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class SellOrderCreatedEvent extends ApplicationEvent {
+    private final Long orderId;
+    private final String ticker;
+    private final int quantity;
+    private final BigDecimal price;
+
+    public SellOrderCreatedEvent(Object source, Long orderId, String ticker, int quantity, BigDecimal price) {
+        super(source);
+        this.orderId = orderId;
+        this.ticker = ticker;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
