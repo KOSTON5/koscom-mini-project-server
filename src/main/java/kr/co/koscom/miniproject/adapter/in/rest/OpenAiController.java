b8e44834ff137bde0e907ad7509517541fdc1051
Geonwoo Pack <packdev937@gmail.com>
2025-01-31 20:14:59 +0900

    refactor (presentation) : DTO 클래스 이름을 변경한다

--- src/main/java/kr/co/koscom/miniproject/adapter/in/rest/OpenAiController.java
+++ src/main/java/kr/co/koscom/miniproject/adapter/in/rest/OpenAiController.java
@@ -1,24 +1,22 @@
 package kr.co.koscom.miniproject.adapter.in.rest;
 
-import kr.co.koscom.miniproject.adapter.out.client.openai.OpenAiRequest;
-import kr.co.koscom.miniproject.adapter.out.client.openai.OpenAiResponse;
-import kr.co.koscom.miniproject.application.dto.response.AnalyzeOrderResponse;
+import kr.co.koscom.miniproject.application.dto.request.AnalyzeTextRequest;
+import kr.co.koscom.miniproject.application.dto.response.AnalyzeTextResponse;
 import kr.co.koscom.miniproject.application.service.OpenAiApplicationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class OpenAiController implements OpenAiControllerDocs {
 
     private final OpenAiApplicationService openAiApplicationService;
 
-    /**
-     * 사용 하지 않는 API
-     */
     @Override
-    public ResponseEntity<OpenAiResponse> analyzeText(OpenAiRequest openAiRequest) {
-        return ResponseEntity.ok(openAiApplicationService.analyzeText(openAiRequest));
+    public ResponseEntity<AnalyzeTextResponse> analyzeText(AnalyzeTextRequest request) {
+        return ResponseEntity.ok(openAiApplicationService.analyzeText(request));
     }
 }
--- src/main/java/kr/co/koscom/miniproject/application/dto/response/AnalyzeOrderResponse.java
+++ src/main/java/kr/co/koscom/miniproject/application/dto/response/AnalyzeOrderResponse.java
@@ -2,10 +2,8 @@ package kr.co.koscom.miniproject.application.dto.response;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 
-@AllArgsConstructor
 @Builder(toBuilder = true)
 public record AnalyzeOrderResponse(
     @NotNull String orderType, @NotNull String ticker, @NotNull String stockName, @NotNull int quantity, @NotNull int price,
